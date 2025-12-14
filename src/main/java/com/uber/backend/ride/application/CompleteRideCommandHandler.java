f85db72c610c15b1ba8721b485f8660de7cdc5ff
Manny <mannysinghb2005@gmail.com>
2025-12-14 22:55:12 +0100

    Establish bidirectional relationship between ride and payment upon completion

--- src/main/java/com/uber/backend/ride/application/CompleteRideCommandHandler.java
+++ src/main/java/com/uber/backend/ride/application/CompleteRideCommandHandler.java
@@ -69,6 +69,9 @@ public class CompleteRideCommandHandler {
                 .build();
 
         paymentRepository.save(payment);
+        
+        // Set bidirectional relationship
+        rideEntity.setPayment(payment);
 
         // Update ride status
         rideEntity.setStatus(RideStatus.COMPLETED);
